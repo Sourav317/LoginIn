fddb75f0e56c2d6963e4c2de7f59065fada36138
helix32 <kediasourav2@gmail.com>
2021-01-25 22:42:00 +0530

    Sign_in page created

--- Controllers/user_controller.js
+++ Controllers/user_controller.js
@@ -2,7 +2,10 @@
 console.log("inside controller");
 module.exports.sign = function(req,res)
 {
-    return res.end('<h1>HI im in controller</h1>')
+    //return res.end('<h1>HI im in controller</h1>')
+    return res.render('User_SignIn',{
+        title : "Sign In"
+    })
 }
 
 //render the Sign In page
